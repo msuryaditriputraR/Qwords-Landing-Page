17b6070c19c6fd6f4e3f095438728fddb254a2ce
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2023-03-05 23:01:16 +0800

    add:  tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+    content: ["./resources/**/*.blade.php"],
+    theme: {
+        extend: {
+            colors: {
+                "main-color": "#ff6d0b",
+            },
+        },
+    },
+    plugins: [],
+};
